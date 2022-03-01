8d38938e383b33f38e40c5bd976c6479a90ec683
fayejahamed <foyezfoez017@gmail.com>
2022-02-28 22:33:49 -0500

    second commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    searchText.value = '';
+    searchField.value = '';
+    // console.log(searchText)
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     // console.log(url)
@@ -9,24 +10,56 @@ const searchPhone = () => {
         .then(res => res.json())
         .then(data => displayData(data.data))
 }
+searchPhone()
 const displayData = phones => {
     // console.log(phone)
     const phoneContainer = document.getElementById('phone-container');
     phones.forEach(phone => {
-        console.log(phone)
+        // console.log(phone)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `  
         <div class="card h-100">
-           <img src="${phone.image}" class="card-img-top w-75" alt="...">
+           <img src="${phone.image}" class="card-img-top w-50 mt-2 mx-auto" alt="...">
             <div class="card-body">
-               <h5 class="card-title">Card title</h5>
-               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to  additional content. This card has even longer content than the first to show that equal
-                height action.</p>
+               <h5 class="card-title">${phone.brand}</h5>
+               <p>${phone.phone_name}</p>
             </div>
-            <button id="explore-button" class="w-50 mx-auto">Explore more</button>
+            <button id="explore-button" onclick="exploreMore('${phone.slug}')" class="w-50 mx-auto">Explore more</button>
         </div> `;
         phoneContainer.appendChild(div)
-    })
-
+    });
 }
+
+const exploreMore = phoneDetail => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${phoneDetail}`;
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayExploreButton(`${data.data}
+         `))
+}
+const displayExploreButton = explore => {
+    // console.log(explore)
+    const exploreResult = document.getElementById('explore-result')
+    const div = document.createElement('div');
+    div.classList.add('explore')
+    div.innerHTML = `
+     <div class="card text-center">
+        <div class="card-header">
+          Featured
+        </div>
+         <div class="card-body">
+           <h5 class="card-title">${explore.others.gps}</h5>
+           <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+         </div>
+    </div> `;
+    exploreResult.appendChild(div)
+}
+
+
+// ${data.data.mainFeatures.chipSet}
+// ${data.data.mainFeatures.displaySize}
+// ${data.data.mainFeatures.displaySize}
+// ${data.data.mainFeatures.memory} 
